fdcd824385b8c1d988a7df684433ef519eed85ae
ixc7 <isaacxpreston7@gmail.com>
2022-02-11 07:26:47 -0500

    lint, add useState

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Header from './header'
 import Review from './review'
 import CardSection from './cardSection'
@@ -5,25 +6,33 @@ import Reply from './reply'
 import { STRESSTXT, truncate } from './utils'
 
 const App = () => {
+  const [state, toggleView] = useState({cards: true, review: false})
+
   return (
     <div className='container'>
-      <Header title='Reviews' />
+      <div onClick={() => { toggleView({cards: !state.cards, review: !state.review }) }}>
+        <Header title='Reviews'/>
+      </div>
 
-      <CardSection />
+      <div className={state.cards ? 'visible' : 'hidden'}>
+        <CardSection />
+      </div>
 
-      <div className='review-container'>
-        <Review
-          title={truncate(STRESSTXT, 80)}
-          stars='5'
-          text={STRESSTXT}
-          author={truncate(STRESSTXT, 18)}
-          date='01/01/2019'
-        />
-        <Reply
-          text={STRESSTXT}
-          author={truncate(STRESSTXT, 18)}
-          date='01/01/2019'
-        />
+      <div className={state.review ? 'visible' : 'hidden'}>
+        <div className='review-container'>
+          <Review
+            title={truncate(STRESSTXT, 80)}
+            stars='5'
+            text={STRESSTXT}
+            author={truncate(STRESSTXT, 18)}
+            date='01/01/2019'
+          />
+          <Reply
+            text={STRESSTXT}
+            author={truncate(STRESSTXT, 18)}
+            date='01/01/2019'
+          />
+        </div>
       </div>
 
     </div>
